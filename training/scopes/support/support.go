c64182f3fb18ac3ecab06b2f4d8b1a705b31a92a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-07 14:12:57 +0200

    Declaration of variable visible globally from another package

---
+++ training/scopes/support/support.go
@@ -0,0 +1,5 @@
+package support
+
+// Variables / functions declared with capital letter are accessible
+// from different package by direct reference like package_name.FunctionName()
+var Conf = "Variable visible when support package imported."
