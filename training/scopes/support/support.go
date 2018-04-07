660d98b49f7ee5d43a9485c704a90820d7ccf00a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-07 14:58:47 +0200

    Playing around publicly accessible getter like functions

--- training/scopes/support/support.go
+++ training/scopes/support/support.go
@@ -2,4 +2,12 @@ package support
 
 // Variables / functions declared with capital letter are accessible
 // from different package by direct reference like package_name.FunctionName()
-var Conf = "Variable visible when support package imported."
+var Conf = "Variable visible when support package imported (public to the scope)"
+var conf = "Variable not visible when support package imported (private to the scope)"
+
+// Setup 'getter' like function for local variable, that cannot be accessed directly
+// with reference to support package. Ex. support.conf local variable call won't
+// succeed.
+func ConfGetter() string {
+	return conf
+}
