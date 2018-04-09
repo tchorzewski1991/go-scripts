087356b7e33b2741f27bad400560f8c023074bde
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-09 22:01:24 +0200

    Example of using pointers in Go with simple meters to yard converter

---
+++ training/pointers/converter/converter.go
@@ -0,0 +1,7 @@
+package converter
+
+const metersToYards float64 = 1.09361
+
+func MetersToYards(meters float64) float64 {
+	return meters * metersToYards
+}
--- training/pointers/main.go
+++ training/pointers/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/tchorzewski1991/go-scripts/training/pointers/converter"
+)
 
 func main() {
 
-	// Fetch memory address by prepending variable with &
+	// Access to memory address of the value referenced by variable could
+	// be achieved easily by using & operator in front of the variable.
 	x := 42
 	fmt.Printf("%p \n", &x)
+
+	// Example of using pointers. Notice that fmt.Scan() function
+	// takes a pointer as an argument rather than value itself.
+	// We want to feed exactly that particular memory slot with user
+	// input.
+	fmt.Print("Enter meters: ")
+
+	var meters float64
+
+	fmt.Scan(&meters)
+	fmt.Printf("%v \n", converter.MetersToYards(meters))
 }
