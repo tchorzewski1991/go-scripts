c3a0615e5e234c8b87cb915308a0c95e23011553
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-10 23:53:20 +0200

    Playing around C-style loops in Go

---
+++ training/loops/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+const breakPoint int = 10
+
+func main() {
+
+	var x int
+
+	// Declaration of function expression that will be used as
+	// conditional statement within for loop. Notice variable x
+	// is visible on anonymous function block and do not need to be
+	// provided explicitly. Default value for x will be resolved
+	// automatically.
+	condition := func () bool {
+		return x < breakPoint
+	}
+
+	// Go do not have 'do - while' kind of loops. Instead we can
+	// use more 'C' style loops like the one below.
+	for condition() {
+		fmt.Printf("x is : %d \n", x)
+		x++
+	}
+}
