54783e2c07dc7448de0f56cddc1342e2bcd53892
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-15 15:00:13 +0200

    Playing around ability to return named values from the function

---
+++ training/functions/named-returns/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	valueToCheck := 3
+
+	// Golang allows for attaching name to the value returned by function.
+	// This is not quite frequently used pattern, but could be interesting
+	// in some cases.
+	divideableByTwo := func (i int) (result bool) {
+		result = i % 2 == 0
+		return
+	}
+
+	// Should return false, as 3 is not divideable by 2.
+	fmt.Println(divideableByTwo(valueToCheck))
+}
