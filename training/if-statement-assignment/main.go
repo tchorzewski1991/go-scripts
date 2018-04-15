d356da40c0ae5acc598285832f4d90d4218b75f9
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-15 12:31:57 +0200

    Implement if statement with function expression declaration local to the 'if' block scope

---
+++ training/if-statement-assignment/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sum := 0
+
+	// Golang allows for variable assignment during  'if' conditional statement
+	// declaration. Notice it is allowed to assign even anonymous function.
+	// The scope of assigned variable is the 'if' block which prevents outer
+	// scope from unnecessary code pollutions.
+
+	for n := 0; n < 1000; n++ {
+		if mod3 := func (i int) bool {return i % 3 == 0}(n); mod3 {
+			sum += n
+		}
+	}
+
+	// Should print sum of all numbers lower than 1000 and dividable by 3.
+	fmt.Println("Sum is:", sum)
+
+}
