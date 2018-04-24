298f930d6ced03b3c9341fe64a1052603299ebe7
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-04-24 23:24:52 +0200

    Finding out two appropriate ways to declare and assign maps

--- training/maps/main.go
+++ training/maps/main.go
@@ -20,5 +20,32 @@ func main() {
 	}
 
 	result := wordsCounter("a", "b", "a", "c")
-	fmt.Printf("Words count: %+v", result)
+	fmt.Printf("Words count: %+v \n", result)
+
+	// You won't be able to make assignment when map is declared in
+	// the way presented below, as it points to nil value
+
+	//var nilMap map[string]string
+	//nilMap["key"] = "value"
+	//fmt.Printf("Assignment won't happen. An error will occure: %v \n", makeMap)
+
+	// Map declaration with build-in make() function won't produce map
+	// that points to nil reference. It allows you to assign any key - value
+	// pair with correct types.
+	var makeMap = make(map[string]string)
+
+	makeMap["firstKey"] = "firstValue"
+	makeMap["lastKey"]  = "lastKey"
+
+	fmt.Printf("Assignment will happen. No errors encountered: %v \n", makeMap)
+
+	// Composite literal is the last technique of declaring new maps that won't
+	// points under the hood to nil reference.
+	compositeLiteralMap := map[string]string{
+		"firstKey": "firstValue",
+		"lastKey":  "lastValue",
+	}
+
+	fmt.Printf("Assignment will happen. No errors encountered: %v \n", compositeLiteralMap)
 }
+
