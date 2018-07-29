91c8a4ca8e08518ccc323850b0f9c8a75c797651
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-07-29 19:20:00 +0200

    Implement simple sorting with usage of sort.StringSlice type

---
+++ training/interfaces/string-slice/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type people []string
+
+func main() {
+	 students := people{"Kelly", "John", "Al", "Cecil"}
+
+	fmt.Println("Unsorted numbers: ", students)
+
+	// sort.StringSlice is a type that defines implicitly
+	// Interface interface. What does it mean is that all
+	// methods required by sort.Sort() are already predefined.
+	// We do need nothing to do on our side to provide sorting
+	// functionality.
+	sort.StringSlice(students).Sort()
+
+	fmt.Println("Sorted students: ", students)
+}
