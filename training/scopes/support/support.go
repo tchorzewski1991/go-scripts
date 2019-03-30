3fd26692b18cf201d2e7dfd33592979a1df5f82f
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2019-03-30 17:01:49 +0100

    Go linting cleanup - training - scopes

--- training/scopes/support/support.go
+++ training/scopes/support/support.go
@@ -2,12 +2,14 @@ package support
 
 // Variables / functions declared with capital letter are accessible
 // from different package by direct reference like package_name.FunctionName()
-var Conf = "Variable visible when support package imported (public to the scope)"
-var conf = "Variable not visible when support package imported (private to the scope)"
 
-// Setup 'getter' like function for local variable, that cannot be accessed directly
-// with reference to support package. Ex. support.conf local variable call won't
-// succeed.
+// Conf represents package level local variable that will be accessible from
+// outside of support package scope.
+var Conf = "Conf variable will be visible (public to the scope)"
+var conf = "conf variable won't be visible (private to the scope)"
+
+// ConfGetter refers to proxy function for package level local variable.
+// It cannot be accessed directly with reference to support package.
 func ConfGetter() string {
 	return conf
 }
